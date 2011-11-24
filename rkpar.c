55e315079ac19bddc434f2c28e5d8e2029f72bc0
Diego Vinicius <eletronzip@gmail.com>
2011-11-24 14:30:19 -0300

    versão realmente com algoritmo paralelizável

--- rkpar.c
+++ rkpar.c
@@ -1,5 +1,3 @@
-/* Runge Kutta for a set of first order differential equations */
-
 #include <stdio.h>
 #include <math.h>
 
@@ -13,15 +11,15 @@ double runge4(double t, double y); /* Runge-Kutta function */
 double F(double x, double y);          /* function for derivatives */
 
 double runge4(double t, double y){
-  double h = dist, hh = h/2, k1, k2, k3, k4;
+  double h = dist, hh = h/2, h3 = h/3, k1, k2, k3, k4;
   int i;
 
   k1 = y;
   k2 = y + hh*F(t + hh, k1);
-  k3 = y + hh*F(t + hh, k2);
-  k4 = y + h*F(t + h, k3);
+  k3 = y + h3*F(t + hh, k1);
+  k4 = y + h3*F(t + hh, k1) + h3*F(t + hh, k3);
 
-  y += 0.33*h*(F(t, y) + 2*F(t + hh, k1) + F(t + hh, k2) + F(t + h, k3));
+  y += h*(-2*F(t + hh, k2) + 1.5*F(t + h3, k3) + 1.5*F(t + 2*h3, k4));
   return y;
 }
 
